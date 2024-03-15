da2aa1ee9a3c310d18adb2d97cfabc2336986846
Mihai <mihai807@mail.ru>
2024-03-15 15:54:53 +0300

    fix

--- src/components/Article/index.tsx
+++ src/components/Article/index.tsx
@@ -1,40 +1,29 @@
 import { Link } from 'react-router-dom';
 import './Article.scss';
+import ArticleItem from './ArticleItem';
 
 const Article = () => {
-    return (
-        <section className="article">
-            <h2 className="article__title">Cтатьи</h2>
-            <div className="article__cards">
-                <Link to="https://www.google.ru/" className="article__card">
-                    <div className="article__card_img">
-                        <img src="https://img.razrisyika.ru/kart/64/252759-gornyy-peyzazh-5.jpg" alt="" />
-                    </div>
-                    <div className="article__text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis eveniet itaque sapiente nisi numquam saepe quidem perferendis
-                        deserunt? Distinctio beatae nisi quas adipisci neque labore qui iusto molestias nam corrupti!
-                    </div>
-                </Link>
-                <Link to="https://www.google.ru/" className="article__card">
-                    <div className="article__card_img">
-                        <img src="https://img.razrisyika.ru/kart/64/252759-gornyy-peyzazh-5.jpg" alt="" />
-                    </div>
-                    <div className="article__text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis eveniet itaque sapiente nisi numquam saepe quidem perferendis
-                        deserunt? Distinctio beatae nisi quas adipisci neque labore qui iusto molestias nam corrupti!
-                    </div>
-                </Link>
-                <Link to="https://www.google.ru/" className="article__card">
-                    <div className="article__card_img">
-                        <img src="https://img.razrisyika.ru/kart/64/252759-gornyy-peyzazh-5.jpg" alt="" />
-                    </div>
-                    <div className="article__text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis eveniet itaque sapiente nisi numquam saepe quidem perferendis
-                        deserunt? Distinctio beatae nisi quas adipisci neque labore qui iusto molestias nam corrupti!
-                    </div>
-                </Link>
-            </div>
-        </section>
-    );
+  return (
+    <section className="article">
+      <h2 className="article__title">Cтатьи</h2>
+      <div className="article__cards">
+        <ArticleItem
+          url="https://www.google.ru/"
+          imgUrl="https://img.razrisyika.ru/kart/64/252759-gornyy-peyzazh-5.jpg"
+          text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis eveniet itaque sapiente nisi numquam saepe quidem perferendis deserunt? Distinctio beatae nisi quas adipisci neque labore qui iusto molestias nam"
+        />
+        <ArticleItem
+          url="https://www.google.ru/"
+          imgUrl="https://img.razrisyika.ru/kart/64/252759-gornyy-peyzazh-5.jpg"
+          text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis eveniet itaque sapiente nisi numquam saepe quidem perferendis deserunt? Distinctio beatae nisi quas adipisci neque labore qui iusto molestias nam"
+        />
+        <ArticleItem
+          url="https://www.google.ru/"
+          imgUrl="https://img.razrisyika.ru/kart/64/252759-gornyy-peyzazh-5.jpg"
+          text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis eveniet itaque sapiente nisi numquam saepe quidem perferendis deserunt? Distinctio beatae nisi quas adipisci neque labore qui iusto molestias nam"
+        />
+      </div>
+    </section>
+  );
 };
 export default Article;
